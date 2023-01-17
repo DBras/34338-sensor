67f477bb3078379791540b04a96d7c63e0bae5d7
DiscountSunTzu <122361049+DiscountSunTzu@users.noreply.github.com>
2023-01-17 13:13:49 +0100

    Motor to for loop

--- sensor-arduino/sensor-arduino.ino
+++ sensor-arduino/sensor-arduino.ino
@@ -21,31 +21,23 @@ void setup() {
   Serial.begin(115200);
 }
 
+uint8_t pins[4] = {PD2, PD3, PD4, PD5};
+
 void loop() {
   int readIn = analogRead(A0);
   Serial.println(readIn);
   //Check for low or high input
   if (readIn > 300) {
     //Start the step loop for the step motor
-    digitalWrite(PD2, HIGH);
-    digitalWrite(PD3, LOW);
-    digitalWrite(PD4, LOW);
-    digitalWrite(PD5, LOW);
-    delay(5);
-    digitalWrite(PD2, LOW);
-    digitalWrite(PD3, HIGH);
-    digitalWrite(PD4, LOW);
-    digitalWrite(PD5, LOW);
-    delay(5);
-    digitalWrite(PD2, LOW);
-    digitalWrite(PD3, LOW);
-    digitalWrite(PD4, HIGH);
-    digitalWrite(PD5, LOW);
-    delay(5);
-    digitalWrite(PD2, LOW);
-    digitalWrite(PD3, LOW);
-    digitalWrite(PD4, LOW);
-    digitalWrite(PD5, HIGH);
-    delay(5);
+    for(int i=0; i<4; i++) {
+      digitalWrite(pins[i], HIGH);
+      if (i-1 >= 0) {
+        digitalWrite(pins[i-1], LOW);
+      }
+      else {
+        digitalWrite(pins[3], LOW);
+      }
+      delay(5);
+    }
   }
 }
