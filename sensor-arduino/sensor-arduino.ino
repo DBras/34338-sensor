f40fb2a157443ea3e81a41a7be8e780a911adc84
DiscountSunTzu <122361049+DiscountSunTzu@users.noreply.github.com>
2023-01-17 11:46:30 +0100

    Adding comments

--- sensor-arduino/sensor-arduino.ino
+++ sensor-arduino/sensor-arduino.ino
@@ -1,31 +1,9 @@
 /*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+ 
 */
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize pins for motor output.
   pinMode(PD2, OUTPUT);
   pinMode(PD3, OUTPUT);
   pinMode(PD4, OUTPUT);
@@ -33,12 +11,12 @@ void setup() {
   Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-int v = analogRead(A0);
-
-Serial.println(v);
-if(v>300){
+  int readIn = analogRead(A0);
+  Serial.println(readIn);
+  //Check for low or high input
+  if (readIn > 300) {
+    //Start the step loop for the step motor
     digitalWrite(PD2, HIGH);
     digitalWrite(PD3, LOW);
     digitalWrite(PD4, LOW);
@@ -59,6 +37,5 @@ if(v>300){
     digitalWrite(PD4, LOW);
     digitalWrite(PD5, HIGH);
     delay(5);
-}
-  
+  }
 }
--- sensor-esp/sensor-esp.ino
+++ sensor-esp/sensor-esp.ino
@@ -1,39 +1,49 @@
+/*
+
+*/
+
 #include <espnow.h>
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
 #include "DHT.h"
+
 #define DHTPIN D2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
-//int tempPin = A0;
+
+//Set output pins
 int noisePin = A0;
 int ledPin = 13;
+
+//Setting initial values
 int tempValue = 0;
 int noiseValue = 0;
 int tickets = 0;
 float avgtemp = 0;
 float avgnoise = 0;
 float avghum = 0;
+
+//WiFi connection parameters
 WiFiClient client;
 const char* ssid = "P7";
 const char* pass = "dtuiot!!";
-unsigned long channelID = 2004080;
-const char* APIKey = "A5HP1PM53222TH9M";
-const char* server = "api.thingspeak.com";
 
+//Message sending structure
 uint8_t broadcastAddress[] = { 0x84, 0xF3, 0xEB, 0x31, 0xD0, 0x74 };
 typedef struct struct_message {
   float avgtemp = 0;
   float avghum = 0;
   float avgnoise = 0;
 } struct_message;
+
+//Incoming message structure
 typedef struct receive_struct_message {
   bool turnOn;
 } receive_struct_message;
-
 struct_message myData;
 receive_struct_message receivedData;
 
+//function finding controller WiFi channel
 int32_t getWiFiChannel(const char* ssid) {
   if (int32_t n = WiFi.scanNetworks()) {
     for (uint8_t i = 0; i < n; i++) {
@@ -45,6 +55,7 @@ int32_t getWiFiChannel(const char* ssid) {
   return 0;
 }
 
+//Sent data status message
 void OnDataSent(uint8_t* mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0) {
@@ -54,6 +65,7 @@ void OnDataSent(uint8_t* mac_addr, uint8_t sendStatus) {
   }
 }
 
+//Send signal to sensor arduino
 void onDataRec(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&receivedData, incomingData, sizeof(receivedData));
   Serial.print("Received data");
@@ -65,6 +77,7 @@ void onDataRec(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
 }
 
 void setup() {
+  //Initialize WiFi
   WiFi.mode(WIFI_STA);
   int32_t channel = getWiFiChannel(ssid);
   WiFi.printDiag(Serial);
@@ -72,21 +85,21 @@ void setup() {
   wifi_set_channel(channel);
   wifi_promiscuous_enable(0);
   WiFi.printDiag(Serial);
+  
   dht.begin();
+  Serial.begin(115200);
+  
   pinMode(ledPin, OUTPUT);
   pinMode(D7, OUTPUT);
-  Serial.begin(115200);
-  //WiFi.begin(ssid, pass);
-  //pinMode(PD2, OUTPUT);
-  //ThingSpeak.begin(client);
+  
 
-  // Init ESP-NOW
+  // Initialize ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
+  // Once ESPNow is successfully Initialized, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
   esp_now_register_send_cb(OnDataSent);
@@ -98,36 +111,37 @@ void setup() {
 }
 
 void loop() {
-
+  //Reading sensors
   delay(20);
   noiseValue = analogRead(noisePin);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float CurrentHumidity = dht.readHumidity();
+  float CurrentTemperature = dht.readTemperature();
   
-
-  avgtemp = avgtemp + t;
+  //Adding current sensor values to previous sensor values
+  avgtemp = avgtemp + CurrentTemperature;
   avgnoise = avgnoise + noiseValue;
-  avghum = avghum + h;
+  avghum = avghum + CurrentHumidity;
 
+  //Check how meny values were added and averaging over them in sets of 500
   if (tickets == 500) {
-
+    //Averaging calculations    
     avgtemp = avgtemp / 500;
     avghum = avghum / 500;
     avgnoise = avgnoise / 500;
 
-    Serial.print("curtemp er ");
-    Serial.println(t);
-    Serial.print("CurNoise er ");
+    //Printing data to serial monitor    
+    Serial.print("Current temp is: ");
+    Serial.println(CurrentTemperature);
+    Serial.print("Current noise is: ");
     Serial.println(noiseValue);
-    Serial.print("CurHumidity er ");
-    Serial.println(h);
+    Serial.print("Current Humidity is: ");
+    Serial.println(CurrentHumidity);
 
-
-    Serial.print("Temperatur er ");
+    Serial.print("Temperature is: ");
     Serial.println(avgtemp);
-    Serial.print("Noise er ");
+    Serial.print("Noise is: ");
     Serial.println(avgnoise);
-    Serial.print("Humidity er ");
+    Serial.print("Humidity is: ");
     Serial.println(avghum);
     Serial.println("");
     Serial.println("");
@@ -138,7 +152,7 @@ void loop() {
     Serial.println("");
     Serial.println("");
 
-
+    //Writing average values to struct
     myData.avgtemp = avgtemp;
     myData.avghum = avghum;
     myData.avgnoise = avgnoise;
@@ -146,11 +160,13 @@ void loop() {
     // Send message via ESP-NOW
     esp_now_send(broadcastAddress, (uint8_t*)&myData, sizeof(myData));
 
+    //Reset values for next batch
     tickets = 0;
     avgtemp = 0;
     avghum = 0;
     avgnoise = 0;
   }
 
+  //Add a ticket to track amount of values
   tickets = tickets + 1;
 }
